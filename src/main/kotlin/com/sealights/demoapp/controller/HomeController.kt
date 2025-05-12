af58ae7403948a9a9133d3bd126567a632bf30a6
s.perez <s.perez@tricentis.com>
2025-05-12 10:23:13 -0500

    [BTQ-61] add default home page

---
+++ src/main/kotlin/com/sealights/demoapp/controller/HomeController.kt
@@ -0,0 +1,15 @@
+package com.sealights.demoapp.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/")
+class HomeController {
+    
+    @GetMapping
+    fun home(): String {
+        return "redirect:/index.html"
+    }
+}
